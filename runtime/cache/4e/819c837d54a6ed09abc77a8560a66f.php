d49f95bbfd73fc34691c4614b2ab72405965f143
qianmoqingfeng <85347325@qq.com>
2022-11-01 20:45:56 +0800

    删除无效文件

--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:1:{s:8:"testhook";a:3:{i:0;a:2:{i:0;s:19:"\addons\edoc\Plugin";i:1;s:8:"testhook";}i:1;a:2:{i:0;s:26:"\addons\more_module\Plugin";i:1;s:8:"testhook";}i:2;a:2:{i:0;s:25:"\addons\one_module\Plugin";i:1;s:8:"testhook";}}}

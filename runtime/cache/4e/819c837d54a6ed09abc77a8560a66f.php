4033e1d4bb27d5b599c73bc069704cfb439f7a3c
qianmoqingfeng <85347325@qq.com>
2022-11-21 17:22:43 +0800

    删除缓存文件

--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:2:{s:8:"testhook";a:3:{i:0;a:2:{i:0;s:19:"\addons\edoc\Plugin";i:1;s:8:"testhook";}i:1;a:2:{i:0;s:26:"\addons\more_module\Plugin";i:1;s:8:"testhook";}i:2;a:2:{i:0;s:25:"\addons\one_module\Plugin";i:1;s:8:"testhook";}}s:16:"InitSaetVueTrait";a:1:{i:0;a:2:{i:0;s:19:"\addons\edoc\Plugin";i:1;s:16:"InitSaetVueTrait";}}}
